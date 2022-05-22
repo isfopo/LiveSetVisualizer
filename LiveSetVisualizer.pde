8af74747a1b6caa8dd73267397ffd5dde211235e
Isaac Poole <isaac@switcherstudio.com>
2022-05-22 01:54:11 -0400

    adds images

--- LiveSetVisualizer.pde
+++ LiveSetVisualizer.pde
@@ -1,38 +1,23 @@
 import oscP5.*;
   
 OscP5 oscP5;
-int dim;
+PImage img;
 float messageValue;
 float size;
 
 void setup() {
+  frameRate(30);
   fullScreen();
+  background(255);
   
   oscP5 = new OscP5(this,12000);
-
-  dim = width/2;
-  background(0);
+  img = loadImage("icons/memory.png");
   colorMode(HSB, 360, 100, 100);
-  noStroke();
-  ellipseMode(RADIUS);
-  frameRate(30);
 }
 
 void draw() {
-  background(0);
-  for (int x = 0; x <= width; x+=dim) {
-    drawGradient(x, height/2);
-  } 
-}
-
-void drawGradient(float x, float y) {
-  int radius = floor( dim * size )/2;
-  float h = messageValue * 360;
-  for (int r = radius; r > 0; --r) {
-    fill(h, 90, 90);
-    ellipse(x, y, r, r);
-    h = (h + 1) % 360;
-  }
+  background(255);
+  image(img, 0, 0, 400, 400);
 }
 
 void oscEvent(OscMessage message) {
--- out/source/LiveSetVisualizer.java
+++ out/source/LiveSetVisualizer.java
@@ -20,38 +20,23 @@ public class LiveSetVisualizer extends PApplet {
 
   
 OscP5 oscP5;
-int dim;
+PImage img;
 float messageValue;
 float size;
 
  public void setup() {
+  frameRate(30);
   /* size commented out by preprocessor */;
+  background(255);
   
   oscP5 = new OscP5(this,12000);
-
-  dim = width/2;
-  background(0);
+  img = loadImage("icons/memory.png");
   colorMode(HSB, 360, 100, 100);
-  noStroke();
-  ellipseMode(RADIUS);
-  frameRate(30);
 }
 
  public void draw() {
-  background(0);
-  for (int x = 0; x <= width; x+=dim) {
-    drawGradient(x, height/2);
-  } 
-}
-
- public void drawGradient(float x, float y) {
-  int radius = floor( dim * size )/2;
-  float h = messageValue * 360;
-  for (int r = radius; r > 0; --r) {
-    fill(h, 90, 90);
-    ellipse(x, y, r, r);
-    h = (h + 1) % 360;
-  }
+  background(255);
+  image(img, 0, 0, 400, 400);
 }
 
  public void oscEvent(OscMessage message) {
